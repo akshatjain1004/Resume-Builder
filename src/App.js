a36a1ad962924da1da74a4a3d0274b8ca321c78a
Dhruv-Mahajan <dhruv.mahajan001@gmail.com>
2023-01-21 13:40:57 +0530

    Login Feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
-
+import Login from "./components/login";
 import "./App.css";
+import { gapi } from "gapi-script";
+import SignIn from "./components/login";
+import Avatar from "@mui/material/Avatar";
+import { Typography } from "@mui/material";
+
+const clientId =
+  "361102340568-4g5oqh4fu0vr5aglh6apigtegs0joelv.apps.googleusercontent.com";
 
 function App() {
+  const [profile, setprofile] = useState([]);
+  const [signin, setsignin] = useState(false);
+
+  function change(profile) {
+    setprofile(profile);
+    setsignin(true);
+    console.log(profile);
+    console.log(profile.imageUrl);
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Body />
+      {!signin && <Login change={change} />}
+      {signin && (
+        <div style={{ padding: "1%", display: "flex" }}>
+          <Typography style={{ padding: "1%" }}>{profile.name}</Typography>
+          <Avatar alt={profile.name} src={profile.imageUrl} />
+        </div>
+      )}
+      {signin && <Header />}
+      {signin && <Body />}
     </div>
   );
 }
